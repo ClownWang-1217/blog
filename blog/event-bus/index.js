a4b0bf690845bd5cb24219d0e25f083d6e8d2369
caleb <caleb.wang@aia.com>
2023-03-06 18:41:03 +0800

    first commit

---
+++ blog/event-bus/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+    console.log('get sth from others')
+   
+    const event = req.body;
+    console.log('event.type:', event.type);
+    axios.post('http://localhost:4000/events', event);
+    axios.post('http://localhost:4001/events', event);
+    axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4003/events', event);
+
+    res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+    console.log('listening on 4005');
+});
